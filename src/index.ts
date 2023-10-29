2036834c08b306c895fb8ae2a51ca6d04a65cb0f
Ibrahim Al-Maradney <maradney@Ibrahims-MacBook-Pro.local>
2023-10-29 07:14:30 +0200

    add simple swagger configurations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
 import express, { Request, Response } from 'express';
 import driverRoutes from './modules/driver/router';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 config();
 
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, res, next) => {
   if (process.env.ENV !== 'testing') {
     console.log(`API called: [${req.method}] ${req.originalUrl}`);
